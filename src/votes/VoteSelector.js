eb6ae950355fb4f58d5c9a9ab6f05604bac0524c
rovack <rovack@walla.co.il>
2019-03-28 08:32:07 -0400

    Add indicator of your current vote

--- src/votes/VoteSelector.js
+++ src/votes/VoteSelector.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 
-export default class VoteSelector extends Component {
+class PartyItem extends Component {
+  onSelect = () => this.props.onSelect(this.props.name);
+
   render() {
-    const { parties, onPartySelected } = this.props;
-
     return (
-      <Dropdown className="p-4">
-        <Dropdown.Toggle size="large">
-          בחירת מפלגה
-        </Dropdown.Toggle>
+      <Dropdown.Item onSelect={this.onSelect}>
+        {this.props.name}
+      </Dropdown.Item>
+    );
+  }
+}
 
-        <Dropdown.Menu>
-          {parties.map((name) => (
-            <Dropdown.Item
-              key={name}
-              onSelect={() => onPartySelected(name)}
-            >
-              {name}
-            </Dropdown.Item>
-          ))}
-        </Dropdown.Menu>
-      </Dropdown>
+export default class VoteSelector extends Component {
+  state = {
+    selectedParty: null,
+  };
+
+  onPartySelected = (partyName) => {
+    this.setState({ selectedParty: partyName }, () =>
+      this.props.onPartySelected(partyName));
+  };
+
+  render() {
+    return (
+      <div className="p-4">
+        <Dropdown>
+          <Dropdown.Toggle size="large">
+            בחירת מפלגה
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            {this.props.parties.map((name) => (
+              <PartyItem key={name} name={name} onSelect={this.onPartySelected} />
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+        {this.state.selectedParty && (
+          <div className="mt-2">
+            <span>אתה מצביע ל:</span>
+            <span className="font-weight-bold mx-1">{this.state.selectedParty}</span>
+          </div>
+        )}
+      </div>
     );
   }
 }
