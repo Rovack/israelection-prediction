692955f962a10dd0e54f87a634864e3cd080c616
rovack <rovack@walla.co.il>
2019-03-28 07:25:51 -0400

    Add charts, and a specific example of one in the vote spread component

--- src/votes/VoteSpread.js
+++ src/votes/VoteSpread.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import { Bar } from 'react-chartjs';
+
+// TODO: Import from /data.
+const parties = [
+  { name: 'כחול לבן' },
+  { name: 'ליכוד' },
+  { name: 'עבודה' },
+];
 
 export default class VoteSpread extends Component {
   render() {
+    const data = {
+      labels: parties.map(({ name }) => name),
+      datasets: [{
+        label: 'מספר קולות',
+        data: [30, 30, 8],
+      }],
+    };
+
     return (
-      <div>
+      <div className="m-4">
+        <h4>
         התפלגות הקולות
+        </h4>
+        <Bar data={data} />
       </div>
     );
   }
